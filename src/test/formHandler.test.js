8a61e9c11c6216209c1d53960bcd18bb613f9be3
mac <mac@Raymonds-MacBook-Pro.local>
2020-04-11 08:22:03 +0100

    latest commit

---
+++ src/test/formHandler.test.js
@@ -0,0 +1,5 @@
+import { handleSubmit } from "../client/js/formHandler";
+
+test("handleSubmit should be defined", () => {
+  expect(handleSubmit).toBeDefined();
+});
---
+++ src/test/sentenceChecker.test.js
@@ -0,0 +1,5 @@
+import { checkForSentence } from "../client/js/sentenceChecker";
+
+test("sentenceChecker should be defined", () => {
+  expect(checkForSentence).toBeDefined();
+});
